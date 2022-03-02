eea462207f778af0af41e5c53a25491aff85b5a9
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-03-02 20:44:35 +0000

    A view for every page created but not implemented because forms are needed

--- WAD2Project10A/OnlyPics/views.py
+++ WAD2Project10A/OnlyPics/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout
-from OnlyPics.models import UserInfo
+from OnlyPics.models import UserInfo, Picture
 from OnlyPics.hcaptcha import verify_hcaptcha_request
 
+#to be used in the template
+def get_comments_according_to_picture(picture):
+    comments = Comment.objects.filter(picture=picture)
+    return comments
+
 def index(request):
+    # top_five_most_liked_pictures = Picture.objects.order_by('-likes')[:5]
+    # context_dic = {}
+    # context_dic['pics'] = top_five_most_liked_pictures
+    # return render(request, 'onlypics/index.html', context=context_dic)
     return HttpResponse(
             """
             Connection terminated.
@@ -13,15 +22,48 @@ def index(request):
             But I'm afraid you've been misinformed.
             """)
 
+def explore(request):
+    picture_list = Picture.objects.order_by('-likes')
+    categories = Category.objects.all()
+
+
+    context_dic = {}
+    context_dic['pictures'] = picture_list
+    context_dic['categories'] = categories
+
+@login_required
+def post_for_sale(request):
+    return HttpResponse("Not yet implemented!")
+    #form = PostForSaleForm()
+    #if request.method == 'POST':
+        #form = PostForSaleForm(request.POST)
+        #if form.is_valid():
+            #form.save(commit=True)
+            #return redirect('/onlypics/')
+        #else
+            #print(form.errors)
+
+     #return render(request, 'onlypics/post_for_sale.html', {'form':form}
+
+@login_required
+def profile(request):
+    user = request.user
+    pictures = Picture.objects.filter(user = user)
+
+@login_required
+def add_tokens(request):
+    return HttpResponse("Not yet implemented!")
+
 def whoami(request):
     if not request.user.is_authenticated:
         return HttpResponse(f"You are not logged in")
     user = request.user
     try:
-        user_info = UserInfo.objects.get(user = user)
+        user_info = UserInfo.objects.get_or_create(user=user, tokens=50)[0]
         return HttpResponse(f"You are {user_info.nickname}")
     except UserInfo.DoesNotExist:
         return HttpResponse("You are logged in but there's no profile of you")
+
 
 # hCaptcha testing
 def vbucks(request):
