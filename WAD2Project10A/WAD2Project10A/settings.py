a295937489a82b5d696474cf44ec19d7ac2f99ba
Ahmad Alwadi <Ahmadalwadi45@gmail.com>
2022-03-09 04:23:05 +0000

    base.css created

--- WAD2Project10A/WAD2Project10A/settings.py
+++ WAD2Project10A/WAD2Project10A/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -75,6 +75,7 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 WSGI_APPLICATION = 'WAD2Project10A.wsgi.application'
 
 
