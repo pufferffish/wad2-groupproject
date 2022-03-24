f3ca2855edbaee6078d20d95b757fddade7ba833
octeep <octeep@pm.me>
2022-03-24 21:17:25 +0000

    auto reload on new commit

--- WAD2Project10A/WAD2Project10A/github.py
+++ WAD2Project10A/WAD2Project10A/github.py
@@ -2,13 +2,12 @@ import git
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from pathlib import Path
 
 @csrf_exempt
 def update(request):
-    if request.method == "POST":
-        repo = git.Repo("/home/octeep/wad2-groupproject")
-        origin = repo.remotes.origin
-        origin.pull()
-        return HttpResponse("Updated code on PythonAnywhere")
-    else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
+    repo = git.Repo("/home/octeep/wad2-groupproject")
+    origin = repo.remotes.origin
+    origin.pull()
+    Path('/var/www/octeep_pythonanywhere_com_wsgi.py').touch()
+    return HttpResponse("Updated code on PythonAnywhere")
