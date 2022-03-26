d6b5813e7548f8ed05928ff99eabccf7b11fe3fa
iliyankalfov <kalfov.iliyan@yahoo.com>
2022-03-26 02:10:25 +0000

    resolving conflicts

--- WAD2Project10A/static/javascript/post_comment.js
+++ WAD2Project10A/static/javascript/post_comment.js
@@ -6,17 +6,10 @@ function escapeHtml(html){
 }
 
 $(document).ready(function() {
-<<<<<<< HEAD
-    $('#post-comment-form').submit(function (e){
-        e.preventDefault();
-        var endpoint = $("#post-comment-form").attr("data-url");
-
-=======
     $('[picture-form]').submit(function(event){
         event.preventDefault();
         var data = new FormData(event.target);
         console.log(data);
->>>>>>> main
         $.ajax({
             type: 'POST',
             url: '/onlypics/post_comment',
@@ -26,15 +19,6 @@ $(document).ready(function() {
             contentType: false,
             data: data,
             success: function (response) {
-<<<<<<< HEAD
-                $("#post-comment-form").trigger('reset');
-                var comment = JSON.stringify(response["comment_text"])
-                var parsed_comment = JSON.parse(comment)
-                var nickname = JSON.stringify(response["user_nickname"]);
-                var parsed_nickname = JSON.parse(nickname)
-                $("#comment-section").append(
-                    `<p style="float: left; margin-left: 20px;"><strong>${parsed_nickname['user_nickname']}</strong> : ${parsed_comment['comment_text']}</p>`
-=======
                 event.target.reset();
                 console.log(response);
                 var nickname = escapeHtml(response["nickname"]);
@@ -43,7 +27,6 @@ $(document).ready(function() {
                 p.style = "float: left; margin-left: 20px;";
                 $("#comment-" + response["uuid"]).append(
                     `<p style="float: left; margin-left: 20px;"><strong>${nickname}</strong> : ${text}</p>`
->>>>>>> main
                 );
             },
             error: function (response) {
